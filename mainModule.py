b4ec43917f778347df54d0b43a903d86dffef905
Victoria Confeiteiro <victoria.confeiteiro1@marist.edu>
2017-03-26 03:10:24 -0400

    Main Module
    Starts game

---
+++ mainModule.py
@@ -0,0 +1,10 @@
+## Almanzi, Nicholas | Lehmann, Margaret | Nuebel, Ryan | Confeiteiro, Victoria
+## CS110 A53, A51, A53, A52
+## Project#1
+
+from GUI import*
+
+def main():
+    game = GUI()
+
+main()
